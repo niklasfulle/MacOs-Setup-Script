06b03d66250eb524d910d1f93d440aa8a6b02910
Niklas Fulle <36989748+niklasfulle@users.noreply.github.com>
2023-07-20 22:41:49 +0200

    Update setup-part-0.sh

--- setup-part-0.sh
+++ setup-part-0.sh
@@ -1,1 +1,11 @@
+# install xcode build tools
+
+xcode-select --install
+
+# install homebrew
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install Iterm2
+brew install --cask iterm2
+
