abf5fa0115cad759509f3f801e74506e8b7e8551
Niklas Fulle <36989748+niklasfulle@users.noreply.github.com>
2023-07-20 22:41:41 +0200

    Update terminal-setup.sh

---
+++ terminal/terminal-setup.sh
@@ -0,0 +1,13 @@
+# install bash
+brew install bash
+
+which bash
+
+sudo nano /etc/shells
+
+# copy /usr/local/bin/bash
+
+chsh /usr/local/bin/bash
+
+# install oh-my-bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
