6084449261bd3b31a053ab20d8f628be637eb449
Niklas Fulle <36989748+niklasfulle@users.noreply.github.com>
2023-07-27 21:32:04 +0200

    Update terminal-setup.sh

--- terminal/terminal-setup.sh
+++ terminal/terminal-setup.sh
@@ -7,7 +7,7 @@ sudo nano /etc/shells
 
 # copy /usr/local/bin/bash
 
-chsh /usr/local/bin/bash
+chsh -s /opt/homebrew/bin/bash
 
 # install oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
